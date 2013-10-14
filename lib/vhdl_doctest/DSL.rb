264691a39dee4a34490b846df3b87fc8b06a9dd9
Tomoya Chiba <tomo.asleep@gmail.com>
2013-10-14 10:25:26 +0900

    Move DSL loader from VhdlDoctest::DSL::TestBench to VhdlDoctest::DSL

--- lib/vhdl_doctest/DSL.rb
+++ lib/vhdl_doctest/DSL.rb
@@ -1,5 +1,13 @@
 require "vhdl_doctest/DSL/base.rb"
-require "vhdl_doctest/DSL/test_bench.rb"
 
 module VhdlDoctest::DSL
+  def self.load_dsl(dut, path)
+    dsl_klass = path.split("/").last.split(".").first.split("_").map(&:capitalize).join
+    Object.const_set(dsl_klass, Class.new(VhdlDoctest::DSL::Base))
+
+    load(path)
+    dsl_class = Object.const_get(dsl_klass).new(dut.ports)
+
+    VhdlDoctest::TestFile.new(dut.entity, dut.ports, dsl_class.cases)
+  end
 end
